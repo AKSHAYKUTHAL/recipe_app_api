1c4bb1493faf70cc1584186ee5e8cec8cb14a502
AKSHAYKUTHAL <akshaykuthal90@gmail.com>
2024-02-01 12:29:26 +0530

    added command testing

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x, y):
+    """ADD x and y return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x and y return result"""
+    return x - y
+
