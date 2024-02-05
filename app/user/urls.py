cb6d14befc7bb87487a8356d71baee828457dd48
AKSHAYKUTHAL <akshaykuthal90@gmail.com>
2024-02-05 11:15:25 +0530

    user APIs

---
+++ app/user/urls.py
@@ -0,0 +1,19 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from user import views
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    # path('me/', views.ManageUserView.as_view(), name='me'),
+]
+
+
+# .as_view() method = django expects a an actual function for parameter here in the url we use this method 
+# converts the class base view in to the supported djanggo view
